89f6fed6d10301c456df4e59d4f5ab372296291d
shinbate <shinbate@paran.com>
2012-11-12 21:17:56 +0900

    add 3 more test

--- src/net/ss/HelloTest.java
+++ src/net/ss/HelloTest.java
@@ -4,5 +4,21 @@ import junit.framework.TestCase;
 
 public class HelloTest extends TestCase {
 
-	
+    public void testGetMessage() throws Exception {               
+    	HelloWorld hello = new HelloWorld();               
+    	assertEquals(ʺHello Worldʺ, hello.getMessage());        
+    	}
+    
+    public void testGetMessageWithName() throws Exception {               
+		  HelloWorld hello = new HelloWorld();               
+		  assertEquals(ʺHello Steveʺ, hello.getMessage(ʺSteveʺ));      
+		  }                
+	  public void testGetMessageWithNameOther() throws Exception {    
+		  HelloWorld hello = new HelloWorld();               
+		  assertEquals(ʺHello Jobsʺ, hello.getMessage(ʺJobsʺ));       
+		  }        
+	  public void testGetMessageWithNameNull() throws Exception { 
+		  HelloWorld hello = new HelloWorld();              
+		  assertEquals(ʺHello Worldʺ, hello.getMessage(null));   
+		  }
 }
